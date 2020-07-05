43badde1ee206528fdcbca5d23d0b97ee0393910
Iqra <iqranaizi1993@gmail.com>
2020-07-05 13:27:24 -0400

    updated

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -8,17 +8,10 @@ import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
 
 public class TweetAPIClient extends TwitterAPIClient {
-    // http://developer.twitter.com/en/docs/tweets/timelines
-    // Tweet Client class that consists of all the different API's of the Twitter tweet
-    // POST: Retrieve and engage with tweets....
 
-    //https://api.twitter.com/1.1/statuses/update.json
     private final String CREATE_TWEET_ENDPOINT="/statuses/update.json";
-    //https://api.twitter.com/1.1/statuses/destroy/:id.json
     private final String DELETE_TWEET_ENDPOINT="/statuses/destroy.json";
-    // https://api.twitter.com/1.1/statuses/user_timeline.json
     private final String GET_USER_TWEET_ENDPOINT="/statuses/user_timeline.json";
-    //https://api.twitter.com/1.1/statuses/home_timeline.json
     private final String GET_HOME_TWEET_ENDPOINT="/statuses/home_timeline.json";
 
 
