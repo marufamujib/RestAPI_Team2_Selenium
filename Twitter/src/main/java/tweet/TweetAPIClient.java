384f8f5d9a730b9765e3da496485020a522c4473
Iqra <iqranaizi1993@gmail.com>
2020-07-05 13:52:21 -0400

    API

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -1,11 +1,6 @@
 package tweet;
 
 import base.TwitterAPIClient;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-
-import static io.restassured.RestAssured.given;
 
 public class TweetAPIClient extends TwitterAPIClient {
 
--- Twitter/src/test/java/testtweet/TestTweetAPIClient.java
+++ Twitter/src/test/java/testtweet/TestTweetAPIClient.java
@@ -1,15 +1,6 @@
 package testtweet;
-
-
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 import tweet.TweetAPIClient;
-
-import java.util.UUID;
 
 public class TestTweetAPIClient {
     private TweetAPIClient tweetAPIClient;
