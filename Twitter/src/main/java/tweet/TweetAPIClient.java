b3699c3446dc4c6f6cb94ed00f76f484dffc6e6e
Ferdousi <fs.kolee@gmail.com>
2020-07-05 23:13:58 -0400

    test cases written

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -20,6 +20,43 @@ public class TweetAPIClient extends TwitterAPIClient {
     private final String GET_USER_TWEET_ENDPOINT="/statuses/user_timeline.json";
     //https://api.twitter.com/1.1/statuses/home_timeline.json
     private final String GET_HOME_TWEET_ENDPOINT="/statuses/home_timeline.json";
+    private final String GET_USER_TWEETS_ENDPOINT="statuses/user_timeline.json";
+    private final String SEARCH_TWEETS_ENDPOINT="/search/tweets.json";
+    private final String GET_AVAILABLE_TRENDS_ENDPOINT="/trends/available.json.json";
+    private final String GET_PLACES_NEAR_ENDPOINT="/geo/reverse_geocode.json";
+    private final String USERS_LOOK_UP_ENDPOINT="/users/lookup.json";
+    private final String FOLLOW_USER_ENDPOINT="/users/lookup.json";
+    private final String ADD_FAVORITES_ENDPOINT="/favorites/create.json";
+
+    public ValidatableResponse searchTweets(String atUsername){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("q", atUsername)
+                .when().get(this.baseUri+this.SEARCH_TWEETS_ENDPOINT)
+                .then();
+    }
+
+    public ValidatableResponse getUserTimelineTweets(){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUri+this.GET_USER_TWEETS_ENDPOINT)
+                .then();
+    }
+
+    // Create a tweet from user's twitter
+    public ValidatableResponse createTweet(String tweet){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("status",tweet)
+                .when().post(this.baseUri+this.CREATE_TWEET_ENDPOINT)
+                .then();
+    }
+
+    // Delete a tweet from the user's twitter
+    public ValidatableResponse deleteTweet( Long tweetId){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id",tweetId)
+                .when().post(this.baseUri+this.DELETE_TWEET_ENDPOINT)
+                .then();
+    }
+
 
 
 
